0860dae33cc3f245c0d6916edc68bcdf5f4e6ebd
Darwin Yusef <wsgestor@gmail.com>
2024-04-26 12:39:51 -0500

    creación de login ccas

--- routes_tailwind/src/App.jsx
+++ routes_tailwind/src/App.jsx
@@ -4,32 +4,43 @@ import './App.css'
 import Home from './Home';
 import About from './About';
 import Contact from './Contact';
+import Login from './Login';
 
 
 function App() {
+  if (localStorage.getItem('login') == 'logueado') {
+    return (
+      <BrowserRouter>
+        <>
+          {/* Generar el menú a las rutas configuradas*/}
+          <nav className='mt-10'>
+            <ul className='flex justify-center gap-3'>
+              <li className='p-2 bg-slate-400 rounded-lg'><Link to="/">Inicio</Link></li>
+              <li className='p-2 bg-slate-400 rounded-lg'><Link to="/about">Acerca de</Link></li>
+              <li className='p-2 bg-slate-400 rounded-lg'><Link to="/contact">Contacto</Link></li>
+            </ul>
+          </nav>
 
-  
-  return (
-    <BrowserRouter>
-      <>
-        {/* Generar el menú a las rutas configuradas*/}
-        <nav className='mt-10'>
-          <ul className='flex justify-center gap-3'>
-            <li className='p-2 bg-slate-400 rounded-lg'><Link to="/">Inicio</Link></li>
-            <li className='p-2 bg-slate-400 rounded-lg'><Link to="/about">Acerca de</Link></li>
-            <li className='p-2 bg-slate-400 rounded-lg'><Link to="/contact">Contacto</Link></li>
-          </ul>
-        </nav>
-
-        {/* Configuración de react router por componente*/}
+          {/* Configuración de react router por componente*/}
+          <Routes>
+            <Route path="/" element={<Login />} />
+            <Route path="/home" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/contact" element={<Contact />} />
+          </Routes>
+        </>
+      </BrowserRouter>
+    )
+  } else {
+    return (
+      <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/contact" element={<Contact />} />
+          <Route path="/" element={<Login />} />
         </Routes>
-      </>
-    </BrowserRouter>
-  )
+      </BrowserRouter >
+    )
+  }
+
 }
 
 export default App
